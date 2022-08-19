7a600e63654d51d8d7d9140780ee5cfc4b7707a0
Wilmer Bordas <108447599+WBordas56@users.noreply.github.com>
2022-08-19 14:34:06 -0600

    Initial Commit
    Co-Authored-By: Daniela Castellanos <73452883+Da836@users.noreply.github.com>

--- app/src/main/java/com/example/examen_3p_movil/ActivityPrincipal.java
+++ app/src/main/java/com/example/examen_3p_movil/ActivityPrincipal.java
@@ -5,13 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Spinner;
 
 public class ActivityPrincipal extends AppCompatActivity {
     ImageView ObjImagen;
     Button btnguardar, btnfoto, btnlista;
-    String Tiempo[] = {"Dia", "Hora"};
+    String tiempo[] = {"Dia", "Hora"};
+    Spinner spinner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,10 +29,16 @@ public class ActivityPrincipal extends AppCompatActivity {
         btnfoto = (Button) findViewById(R.id.btnFoto);
         btnlista = (Button) findViewById(R.id.btnLista);
         ObjImagen = (ImageView) findViewById(R.id.ObjImagen);
+        spinner = (Spinner) findViewById(R.id.spTiempo);
+
+        ArrayAdapter <String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, tiempo);
+        spinner.setAdapter(adapter);
     }
 
     private void onClickLista(View view) {
         Intent regis = new Intent(getApplicationContext(), ActivityLista.class);
         startActivity(regis);
     }
+
+
 }
