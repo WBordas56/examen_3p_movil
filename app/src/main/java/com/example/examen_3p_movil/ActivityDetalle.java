bff2f7725934e254f13ab62c874adf1b64d035f0
Wilmer Bordas <108447599+WBordas56@users.noreply.github.com>
2022-08-19 11:00:51 -0600

    Initial Commit

---
+++ app/src/main/java/com/example/examen_3p_movil/ActivityDetalle.java
@@ -0,0 +1,14 @@
+package com.example.examen_3p_movil;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityDetalle extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalle);
+    }
+}
--- app/src/main/java/com/example/examen_3p_movil/ActivityPrincipal.java
+++ app/src/main/java/com/example/examen_3p_movil/ActivityPrincipal.java
@@ -2,13 +2,33 @@ package com.example.examen_3p_movil;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class ActivityPrincipal extends AppCompatActivity {
-
+    ImageView ObjImagen;
+    Button btnguardar, btnfoto, btnlista;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+        Init();
+
+        btnlista.setOnClickListener(this::onClickLista);
+    }
+
+    private void Init() {
+        btnguardar = (Button) findViewById(R.id.btnGuardar);
+        btnfoto = (Button) findViewById(R.id.btnFoto);
+        btnlista = (Button) findViewById(R.id.btnLista);
+        ObjImagen = (ImageView) findViewById(R.id.ObjImagen);
+    }
+
+    private void onClickLista(View view) {
+        Intent regis = new Intent(getApplicationContext(), ActivityLista.class);
+        startActivity(regis);
     }
 }
