a191541967d5945caeec8d8e59ab2a79ed6a688e
Wilmer Bordas <108447599+WBordas56@users.noreply.github.com>
2022-08-19 09:24:45 -0600

    Initial commit

---
+++ app/src/main/java/com/example/examen_3p_movil/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.examen_3p_movil;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    ImageView imageLogo;
+    Animation animation1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        init();
+
+        imageLogo.setAnimation(animation1);
+        new Handler().postDelayed(this::run, 4000);
+    }
+
+    private void init(){
+        imageLogo = findViewById(R.id.imageViewLogo);
+        animation1 = AnimationUtils.loadAnimation(this, R.anim.scroll_up);
+    }
+
+    private void run() {
+        Intent principal = new Intent(getApplicationContext(), ActivityPrincipal.class);
+        startActivity(principal);
+        finish();
+    }
+}
